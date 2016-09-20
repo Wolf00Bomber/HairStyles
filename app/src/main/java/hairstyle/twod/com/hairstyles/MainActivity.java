b1c4e33f8ef3ddfd30bc43cb7212429890470989
Sri Krishna <srikrishna.nunna@gmail.com>
2016-09-21 00:24:26 +0530

    Commited the first Blank Activity.

---
+++ app/src/main/java/hairstyle/twod/com/hairstyles/MainActivity.java
@@ -0,0 +1,13 @@
+package hairstyle.twod.com.hairstyles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
